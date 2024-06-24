2fea7d2b435ab9855cf010599c36c30e566c5be1
vinodbadiger <vinodbadiger321@gmail.com>
2024-06-24 10:26:47 +0530

    files updated

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return ' this is vinod I am from xcel crop'
+    return ' this is vinod I am from xcel crop '
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8080)
